423f5911bf0fe5f3792529f5274a9cc3819a4dd6
olanmj <olanm@stockton.edu>
2015-09-29 09:24:06 -0400

    CSIS 4135 Lab 4 sample code

---
+++ MVC1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using MVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            Person p = new Person { FirstName = "Sam", LastName = "Smith", BirthDate = new DateTime(1985, 6, 15) };
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult Test(Person newPerson)
+        {
+            bool ok = ModelState.IsValid;
+return View(newPerson);
+        }
+    }
+}
---
+++ MVC1/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
